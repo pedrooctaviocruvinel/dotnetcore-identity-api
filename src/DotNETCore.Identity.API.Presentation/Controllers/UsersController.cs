0a7d3785107428b66fa179ee277dfabf05d7eb74
pedrooctaviocruvinel <pedrooctaviocruvinel@gmail.com>
2023-12-18 11:14:46 -0300

    presentation project created

---
+++ src/DotNETCore.Identity.API.Presentation/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNETCore.Identity.API.Presentation.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    [HttpGet("{id}")]
+    public async Task<IActionResult> RetrieveById([FromRoute] string id) =>
+        Ok();
+}
